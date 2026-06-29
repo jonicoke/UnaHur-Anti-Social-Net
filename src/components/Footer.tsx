f1ed8b87749acb9a6f6149e34f994fc6fabf83f3
jonicoke <cokejonathang@gmail.com>
2026-06-29 13:03:13 -0300

    cambios en navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,8 @@ function HomeFooter() {
     return (
         <footer className="home-footer" >
             <div className="footer-links">
-                <a href="#">Acerca de</a>
+                <a href="#">Sobre nosotros</a>
                 <a href="#">Centro de ayuda</a>
-                <a href="#">Normas de la comunidad</a>
                 <a href="#">Privacidad y condiciones</a>
                 <a href="#">Etiquetas</a>
                 <a href="#">Proyectos</a>
